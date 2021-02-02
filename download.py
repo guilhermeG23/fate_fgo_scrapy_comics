6bef906da076bf1684a380c745c717b1fc038518
guilhermeG23 <guilhermebrechot@gmail.com>
2021-02-02 09:04:43 -0300

    Adicionado a criacao de um PDF de todas as imagens para um BOOK

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import wget
 import requests
 import os
+import img2pdf
+from PIL import Image
 
 #Links para o site
 url = "https://fate-go.us/manga_fgo2/"
@@ -22,19 +24,22 @@ for i in titulos:
         a = str(i).split("/")
         comics.append(a[3])
 
-#Criar pasta
+#Criar pastas necessarias
 if os.path.exists("comics") == False:
     os.mkdir("comics")
+
+if os.path.exists("comics_convertidos") == False:
+    os.mkdir("comics_convertidos")
 
 #Buscar todos os valores do comics
 ja_existem = os.listdir("comics")
 
 #Confirmar se ja existe o logo
 try:
-    ja_existem.index("logo.png")
+    ja_existem.index("comic00.png")
 except:
     wget.download(logo, bar=None)
-    os.rename("logo.png", "comics/logo.png")
+    os.rename("logo.png", "comics/comic00.png")
 
 #Download dos comics
 for comic in comics:
@@ -56,3 +61,26 @@ for comic in comics:
             url = "https://fate-go.us/manga_fgo2/images/{}/{}.png".format(comic, comic)
             wget.download(url, bar=None)
             os.rename("{}.png".format(comic), "comics/{}.png".format(comic))
+
+"""
+Convertendo as imagens
+"""
+for img in ja_existem:
+    im = Image.open("comics\\{}".format(img))
+    rgb_im = im.convert('RGB')
+    rgb_im.save('comics_convertidos\\{}.jpg'.format(img.split(".")[0]))
+
+"""
+Buscando o caminho das imagens
+"""
+ja_existem = os.listdir("comics_convertidos")
+lista_imagens = []
+for img in ja_existem:
+    lista_imagens.append("comics_convertidos\{}".format(img))
+
+"""
+Convertendo em pdf
+"""
+print(lista_imagens)
+with open("comics_fate.pdf","wb") as f:
+	f.write(img2pdf.convert(lista_imagens))     
